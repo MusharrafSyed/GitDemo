638c8b740a02bff60dd9e05e69f1eac41fafb743
Musharraf <musharrafalisyed@gmail.com>
2021-10-23 11:56:20 -0400

    This is my first commit in the github folder

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeApps() {
+		System.out.println("NativeApps");
+		
+	}
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+		
+	}
+	
+
+}
