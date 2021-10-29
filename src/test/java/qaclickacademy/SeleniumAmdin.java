b7a5f1e33d843df6bc37580755d01ca611e42566
Musharraf <musharrafalisyed@gmail.com>
2021-10-28 22:24:56 -0400

    Final commit by asian development team

---
+++ src/test/java/qaclickacademy/SeleniumAmdin.java
@@ -0,0 +1,31 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	
+	@Test
+	public void NativeApps() {
+		System.out.println("NativeApps");
+		System.out.println("NativeAppsdfasdfsaf");
+	}
+	
+		
+	@Test
+	public void NativeApps2() {
+		System.out.println("NativeAppsss");
+		System.out.println("NativeAppsdfasdfsasssf");
+	}
+	
+	
+	@Test
+	public void IOSApps() {
+		System.out.println("IOSApps");
+		System.out.println("Androis");
+		System.out.println("blackberrry");
+		System.out.println("Samsung");
+		
+	}
+	
+
+}
